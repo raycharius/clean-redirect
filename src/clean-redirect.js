59c5de6c5f53aa2b62b8f31538c53da3844d2f09
Ray East <raydeast@gmail.com>
2020-11-09 00:32:57 +0200

    :hammer: Refactor CleanRedirect class

--- src/clean-redirect.js
+++ src/clean-redirect.js
@@ -1,14 +1,13 @@
-const helper = require('./helper');
+const CleanRedirectUrl = require('./clean-redirect-url');
 const validator = require('./validator');
 const {
   www,
   slash,
-  blankString,
   https,
 } = require('./constants');
 
 class CleanRedirect {
-  constructor(requestData, config) {
+  constructor({ method, ...urlData }, config) {
     this.config = {
       forceHttps: config.forceHttps || false,
       toWww: config.toWww || false,
@@ -16,77 +15,67 @@ class CleanRedirect {
       toLowerCase: config.toLowerCase || false,
       removeTrailingSlash: config.removeTrailingSlash || false,
       persistQueryString: config.persistQueryString || false,
+      persistHash: config.persistHash || false,
+      alwaysPassFullUrl: config.alwaysPassFullUrl || false,
       redirectType: config.redirectType || null,
     };
 
-    validator.validateConfig(this.config);
-    Object.freeze(this.config);
+    this.method = method;
 
-    this.originalRequestData = {
-      method: requestData.method,
-      protocol: requestData.protocol,
-      hostname: requestData.hostname,
-      path: helper.parsePath(requestData.originalUrl),
-      queryString: helper.parseQueryString(requestData.originalUrl),
-    };
+    this.sourceUrl = new CleanRedirectUrl(urlData);
 
-    validator.validateRequestData(requestData);
-    Object.freeze(this.originalRequestData);
-
-    this.mutatedRequestData = this.mutateRequestData();
-
-    validator.validateRequestData(this.mutatedRequestData);
-    Object.seal(this.mutatedRequestData);
+    this.targetUrl = new CleanRedirectUrl(urlData)
+      .setProtocol(this.getTargetProtocol())
+      .setHostname(this.getTargetHostname())
+      .setPath(this.getTargetPath());
 
     this.customRedirects = config.customRedirects || null;
 
-    validator.validateCustomRedirects(this.customRedirects);
-
     this.pathOverride = null;
-
-    // If there is a custom redirect function, call it at end of constructor
 
     if (this.customRedirects) {
       this.customRedirects(this);
     }
   }
 
-  mutateRequestData() {
-    return {
-      method: this.originalRequestData.method,
-      protocol: this.mutateProtocol(this.originalRequestData.protocol, this.config),
-      hostname: this.mutateHostname(this.originalRequestData.hostname, this.config),
-      path: this.mutatePath(this.originalRequestData.path, this.config),
-      queryString: this.mutateQueryString(this.originalRequestData.queryString, this.config),
-    };
+  /**
+   * @private
+   */
+
+  getTargetProtocol() {
+    return this.config.forceHttps ? https : this.sourceUrl.protocol;
   }
 
-  mutateProtocol() {
-    return this.config.forceHttps ? https : this.originalRequestData.protocol;
-  }
+  /**
+   * @private
+   */
 
-  mutateHostname() {
+  getTargetHostname() {
     if (this.config.toWww) {
-      return this.originalRequestData.hostname.startsWith(www)
-        ? this.originalRequestData.hostname
-        : `${www}${this.originalRequestData.hostname}`;
+      return this.sourceUrl.hostname.startsWith(www)
+        ? this.sourceUrl.hostname
+        : `${www}${this.sourceUrl.hostname}`;
     }
 
     if (this.config.toNaked) {
-      const startWwwIndex = this.originalRequestData.hostname.indexOf(www);
+      const startWwwIndex = this.sourceUrl.hostname.indexOf(www);
 
       return (startWwwIndex >= 0)
-        ? this.originalRequestData.hostname.slice(4)
-        : this.originalRequestData.hostname;
+        ? this.sourceUrl.hostname.slice(4)
+        : this.sourceUrl.hostname;
     }
 
-    return this.originalRequestData.hostname;
+    return this.sourceUrl.hostname;
   }
 
-  mutatePath() {
+  /**
+   * @private
+   */
+
+  getTargetPath() {
     const mutatedPath = this.config.toLowerCase
-      ? this.originalRequestData.path.toLowerCase()
-      : this.originalRequestData.path;
+      ? this.sourceUrl.path.toLowerCase()
+      : this.sourceUrl.path;
     const pathLength = mutatedPath.length;
     const hasTrailingSlash = mutatedPath.charAt(pathLength - 1) === slash;
 
@@ -97,153 +86,108 @@ class CleanRedirect {
     return mutatedPath;
   }
 
-  // The persistQueryString option is here since by default, Express does not persist, but there are cases where this is necessary
-
-  mutateQueryString() {
-    return this.config.persistQueryString ? this.originalRequestData.queryString : blankString;
+  requiresRedirect() {
+    return this.sourceUrl.url !== this.targetUrl.url || Boolean(this.pathOverride);
   }
 
   getRedirectUrl() {
-    // pathOverride is used to provide an interface to use certain features of the res.redirect func, such as res.redirect('back') or res.redirect('..')
-
     if (this.pathOverride) {
       return this.pathOverride;
     }
 
-    return `${this.getProtocolAndHostnameUrlString()}${this.mutatedRequestData.path}${this.getQueryStringUrlString()}`;
-  }
+    const protocolDiffers = this.sourceUrl.protocol !== this.targetUrl.protocol;
+    const hostnameDiffers = this.sourceUrl.hostname !== this.targetUrl.hostname;
+    const fullPath = this.targetUrl.getFullPath({
+      includeQueryString: this.config.persistQueryString,
+      includeHash: this.config.persistHash,
+    });
 
-  concatFullUrl({
-    protocol, hostname, path, queryString,
-  }) {
-    return `${protocol}://${hostname}${path}${queryString.length > 0 ? `?${queryString}` : queryString}`;
-  }
-
-  // Checking to see if either protocol or host have changed, as not to always pass in a full URL, to not break Express redirect arg logic
-
-  getProtocolAndHostnameUrlString() {
-    const protocolDiffers = this.originalRequestData.protocol !== this.mutatedRequestData.protocol;
-    const hostnameDiffers = this.originalRequestData.hostname !== this.mutatedRequestData.hostname;
-
-    if (protocolDiffers) {
-      return `${this.mutatedRequestData.protocol}://${this.mutatedRequestData.hostname}`;
+    if (protocolDiffers || this.config.alwaysPassFullUrl) {
+      return `${this.targetUrl.protocol}://${this.targetUrl.hostname}${fullPath}`;
     }
 
-    if (hostnameDiffers) {
-      return this.mutatedRequestData.hostname;
-    }
-
-    return blankString;
-  }
-
-  // If it is blank, do not concat the ?
-
-  getQueryStringUrlString() {
-    return this.mutatedRequestData.queryString === blankString
-      ? blankString
-      : `?${this.mutatedRequestData.queryString}`;
-  }
-
-  // These setter methods are used to control the redirect if a function has been passed in for custom redirect logic
-
-  setMethod(method) {
-    validator.validateIsString(method);
-
-    this.mutatedRequestData.method = method;
-
-    return this;
-  }
-
-  setProtocol(protocol) {
-    validator.validateIsString(protocol);
-
-    this.mutatedRequestData.protocol = protocol;
-
-    return this;
-  }
-
-  setHostname(hostname) {
-    validator.validateIsString(hostname);
-
-    this.mutatedRequestData.hostname = hostname;
-
-    return this;
-  }
-
-  setPath(path) {
-    validator.validateIsString(path);
-
-    this.mutatedRequestData.path = path;
-
-    return this;
-  }
-
-  setQueryString(queryString) {
-    validator.validateIsString(queryString);
-
-    this.mutatedRequestData.queryString = queryString;
-
-    return this;
-  }
-
-  // Used to decide in custom redirect logic function whether or not to persist the query string
-
-  setPersistQueryString(bool) {
-    validator.validateIsBoolean(bool);
-
-    this.config.persistQueryString(bool);
-
-    return this;
-  }
-
-  // Used to change code in custom redirect logic function
-
-  setRedirectType(code) {
-    validator.validateRedirectCode(code);
-
-    this.config.redirectType = code;
-
-    return this;
-  }
-
-  // pathOverride is used to provide an interface to use certain features of the res.redirect func, such as res.redirect('back') or res.redirect('..')
-
-  setPathOverride(path) {
-    validator.validateIsString(path);
-
-    this.pathOverride = path;
-
-    return this;
-  }
-
-  // Used to get the mutated values inside the function with custom redirects
-
-  get method() {
-    return this.mutatedRequestData.method;
+    return hostnameDiffers ? `${this.targetUrl.hostname}${fullPath}` : fullPath;
   }
 
   get protocol() {
-    return this.mutatedRequestData.protocol;
+    return this.targetUrl.protocol;
   }
 
   get hostname() {
-    return this.mutatedRequestData.hostname;
+    return this.targetUrl.hostname;
   }
 
   get path() {
-    return this.mutatedRequestData.path;
+    return this.targetUrl.path;
   }
 
   get queryString() {
-    return this.mutatedRequestData.queryString;
+    return this.targetUrl.queryString;
   }
 
   get redirectType() {
     return this.config.redirectType;
   }
 
-  get requiresRedirect() {
-    return this.concatFullUrl(this.originalRequestData) !== this.concatFullUrl(this.mutatedRequestData) || Boolean(this.pathOverride);
+  setProtocol(protocol) {
+    return this.callTargetUrlMethod('setProtocol', protocol);
+  }
+
+  setHostname(hostname) {
+    return this.callTargetUrlMethod('setHostname', hostname);
+  }
+
+  setPath(path) {
+    return this.callTargetUrlMethod('setPath', path);
+  }
+
+  setQueryString(queryString) {
+    return this.callTargetUrlMethod('setQueryString', queryString);
+  }
+
+  setHash(hash) {
+    return this.callTargetUrlMethod('setHash', hash);
+  }
+
+  setPersistQueryString(bool) {
+    validator.validateIsBoolean(bool);
+    return this.setConfigProperty('persistQuery', bool);
+  }
+
+  setPersistHash(bool) {
+    validator.validateIsBoolean(bool);
+    return this.setConfigProperty('persistHash', bool);
+  }
+
+  setRedirectType(code) {
+    validator.validateRedirectCode(code);
+    return this.setConfigProperty('redirectType', code);
+  }
+
+  setPathOverride(path) {
+    validator.validateIsString(path);
+    return this.setConfigProperty('pathOverride', path);
+  }
+
+  /**
+   * @private
+   */
+
+  setConfigProperty(property, value) {
+    this.config[property] = value;
+
+    return this;
+  }
+
+  /**
+   * @private
+   */
+
+  callTargetUrlMethod(method, value) {
+    validator.validateIsString(value);
+    this.targetUrl[method](value);
+
+    return this;
   }
 }
 
