aa74468ce6f3b5595d027ad5b8e6af50dcbeb4fd
Ray East <raydeast@gmail.com>
2020-11-11 23:14:14 +0200

    :zap: Add validators

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,23 @@
 const CleanRedirectError = require('./clean-redirect-error');
 
+const validateBooleanCleanRedirectConfigOptions = ({ redirectType, ...config }) => {
+  Object.keys(config).forEach((key) => {
+    const value = config[key];
+
+    if (typeof value !== 'boolean') {
+      throw new CleanRedirectError(`Expected a value of type 'boolean' for options '${key}'. Instead received type '${typeof typeof value}'`);
+    }
+  });
+};
+
+const validateRedirectCode = (code) => {
+  const validRedirectCodes = [301, 302];
+
+  if (!validRedirectCodes.includes(code)) {
+    throw new CleanRedirectError('Option \'redirectType\' must have an integer value of either 301 or 302');
+  }
+};
+
 const validateCleanRedirectConfig = (config) => {
   validateBooleanCleanRedirectConfigOptions(config);
 
@@ -10,61 +28,42 @@ const validateCleanRedirectConfig = (config) => {
   validateRedirectCode(config.redirectType);
 };
 
-const validateRedirectCode = (code) => {
-  const validRedirectCodes = [301, 302];
+const validateCleanRedirectUrlArgs = (urlData) => {
+  const keys = ['protocol', 'hostname', 'uri'];
 
-  if (!validRedirectCodes.includes(code)) {
-    throw new CleanRedirectError('Option \'redirectType\' must have an integer value of either 301 or 302.');
-  }
-};
+  keys.forEach((key) => {
+    const value = urlData[key];
 
-const validateBooleanCleanRedirectConfigOptions = ({ redirectType, ...config }) => {
-  Object.keys(config).forEach((key) => {
-    const value = config[key];
-
-    if (typeof value !== 'boolean') {
-      throw new CleanRedirectError(`Expected a value of type 'boolean' for options '${key}'. Instead received type ${typeof typeof value}.`);
-    }
-  });
-}
-
-const validateRequestData = (requestData) => {
-  Object.keys(requestData).forEach((key) => {
-    const value = requestData[key];
-
-    if (value === 'undefined') {
-      throw new CleanRedirectError(`Missing a value for request property '${key}'.`);
-    }
-
-    if (typeof value !== 'string') {
-      throw new CleanRedirectError(`Expected a value of type 'string' for request property ${key}. Instead received type ${typeof value}.`);
+    if (value === 'undefined' || typeof value !== 'string') {
+      throw new CleanRedirectError(`CleanRedirectError requires a string value for parameter '${key}', received a value with type '${typeof value}'`);
     }
   });
 };
 
 const validateIsString = (string) => {
   if (typeof string !== 'string') {
-    throw new CleanRedirectError(`Expected a value of type 'string'. Instead received type ${typeof string}.`);
+    throw new CleanRedirectError(`Expected a value of type 'string'. Instead received type '${typeof string}'`);
   }
 };
 
 const validateIsBoolean = (bool) => {
   if (typeof bool !== 'boolean') {
-    throw new CleanRedirectError(`Expected a value of type 'boolean'. Instead received type ${typeof bool}.`);
+    throw new CleanRedirectError(`Expected a value of type 'boolean'. Instead received type '${typeof bool}'`);
   }
 };
 
 const validateCustomRedirects = (customRedirects) => {
   if (customRedirects && typeof customRedirects !== 'function') {
-    throw new CleanRedirectError('When set, the option \'customRedirects\' must either a function.');
+    throw new CleanRedirectError('When set, the option \'customRedirects\' must either a function');
   }
 };
 
 module.exports = {
   validateCleanRedirectConfig,
-  validateRequestData,
   validateIsString,
   validateRedirectCode,
   validateIsBoolean,
   validateCustomRedirects,
+  validateCleanRedirectUrlArgs,
+  validateBooleanCleanRedirectConfigOptions,
 };
